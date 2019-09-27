cdbb37fbf2e8b678473340d2a19fbff7e2b515e1
Marita Horpe <mhorpe@vo-j2.21-school.ru>
2019-09-27 18:39:27 +0300

    upd

--- get_next_line.c
+++ get_next_line.c
@@ -6,17 +6,38 @@
 /*   By: mhorpe <mhorpe@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/09/23 16:12:45 by mhorpe            #+#    #+#             */
-/*   Updated: 2019/09/23 17:18:27 by mhorpe           ###   ########.fr       */
+/*   Updated: 2019/09/27 18:05:58 by mhorpe           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-int		get_next_line(const int fd, char **line)
+/*static int		temp_function_one(int i, char *temp)
+{
+	ft_strdel(&temp);
+	if (i == -1)
+		return (i);
+	return (0);
+}
+
+static void		temp_function_two(char *fds, char ***line)
+{
+	int				test;
+
+	test = ft_strchr(fds, '\n') > 0;
+	if (test)
+		**line = ft_strsub(fds, 0, ft_strchr(fds, '\0') - fds);
+	else
+		**line = ft_strdup(fds);
+	*fds = *ft_strsub(fds, (unsigned int)(ft_strlen(**line) + test),\
+		(ft_strlen(fds) - (ft_strlen(**line) + test)));
+}*/
+
+int				get_next_line(const int fd, char **line)
 {
 	static char		*fds[10000];
 	char			buff[BUFF_SIZE + 1];
-	char			i;
+	int				i;
 	char			*temp;
 
 	if (fd < 0 || !line || read(fd, buff, 0) < 0 || BUFF_SIZE < 1)
@@ -28,5 +49,9 @@ int		get_next_line(const int fd, char **line)
 		fds[fd] = ft_strjoin(fds[fd], buff);
 		free(&temp);
 	}
-	
+	/*if ((i == -1 || !*(temp = fds[fd])))
+		return (temp_function_one(i, temp));
+	temp_function_two(fds[fd], &line);
+	ft_strdel(&temp);*/
+	return (1);
 }
--- get_next_line.h
+++ get_next_line.h
@@ -6,16 +6,16 @@
 /*   By: mhorpe <mhorpe@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/09/23 16:04:32 by mhorpe            #+#    #+#             */
-/*   Updated: 2019/09/23 16:23:52 by mhorpe           ###   ########.fr       */
+/*   Updated: 2019/09/27 17:58:42 by mhorpe           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 # define BUFF_SIZE 64
-#include <stdlib.h>
-#include <unistd.h>
-#include "libft/libft.h"
+# include <stdlib.h>
+# include <unistd.h>
+# include "libft/libft.h"
 
 int		get_next_line(const int fd, char **line);
 #endif
