b73e70d50a4cd71d27310f436c0cbd52734b2e87
Marita Horpe <mhorpe@vo-f2.21-school.ru>
2019-10-03 17:26:22 +0300

    upd

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: mhorpe <mhorpe@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/09/23 16:12:45 by mhorpe            #+#    #+#             */
-/*   Updated: 2019/10/03 13:07:17 by mhorpe           ###   ########.fr       */
+/*   Updated: 2019/10/03 17:24:57 by mhorpe           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,31 +20,44 @@ static int		temp_function_one(int i)
 	return (0);
 }
 
-int	get_next_line(const int fd, char **line)
+static char		*temp_function_two(char *fds, char **line)
 {
-	static char	*c[2147483647];
-	char		buffer[BUFF_SIZE + 1];
-	char		*tmp;
-	ssize_t		i;
-	int			endl;
+	int		test;
 
-	if (fd < 0 || (!c[fd] && !(c[fd] = ft_strnew(1))) || !line)
-		return (-1);
-	while (!ft_strchr(c[fd], '\n') && (i = read(fd, buffer, BUFF_SIZE)) > 0)
-	{
-		buffer[i] = '\0';
-		tmp = c[fd];
-		c[fd] = ft_strjoin(c[fd], buffer);
-		ft_strdel(&tmp);
-	}
-	if (i == -1 || !*(tmp = c[fd]))
-		return (temp_function_one(i));
-	if ((endl = (ft_strchr(c[fd], '\n') > 0)))
-		*line = ft_strsub(c[fd], 0, ft_strchr(c[fd], '\n') - c[fd]);
+	if (ft_strchr(fds, '\n') > 0)
+		test = 1;
 	else
-		*line = ft_strdup(c[fd]);
-	c[fd] = ft_strsub(c[fd], (unsigned int)(ft_strlen(*line) + endl),
-			(size_t)(ft_strlen(c[fd]) - (ft_strlen(*line) + endl)));
-	ft_strdel(&tmp);
-	return (!(!c[fd] && !ft_strlen(*line)));
+		test = 0;
+	if (test == 1)
+		*line = ft_strsub(fds, 0, ft_strchr(fds, '\n') - fds);
+	else
+		*line = ft_strdup(fds);
+	fds = ft_strsub(fds, (unsigned int)(ft_strlen(*line) + test),
+			(size_t)(ft_strlen(fds) - (ft_strlen(*line) + test)));
+	return (fds);
+}
+
+int				get_next_line(const int fd, char **line)
+{
+	static char	*fds[10000];
+	char		buff[BUFF_SIZE + 1];
+	char		*temp;
+	int			i;
+
+	if (fd < 0 || (!fds[fd] && !(fds[fd] = ft_strnew(1))) || !line)
+		return (-1);
+	while (!ft_strchr(fds[fd], '\n') && (i = read(fd, buff, BUFF_SIZE)) > 0)
+	{
+		buff[i] = '\0';
+		temp = fds[fd];
+		fds[fd] = ft_strjoin(fds[fd], buff);
+		ft_strdel(&temp);
+	}
+	if (i == -1 || !*(temp = fds[fd]))
+		return (temp_function_one(i));
+	fds[fd] = temp_function_two(fds[fd], line);
+	ft_strdel(&temp);
+	if (!ft_strlen(*line) && !fds[fd])
+		return (0);
+	return (1);
 }
